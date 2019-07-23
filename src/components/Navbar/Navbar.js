f66da5a8a4e2e05e2059fbf938836ccf8fc61fbf
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-07-23 14:32:37 -0700

    fixed className in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,21 +5,21 @@ import React from "react";
 
 const Navbar = (props) => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/">Clicky Game <span class="sr-only">(current)</span></a>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="/">Clicky Game <span className="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         {/* <NavMessage score={props.score} topScore={props.topScore} /> */}
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Score: {props.score} | Top Score: {props.topScore} </a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Score: {props.score} | Top Score: {props.topScore} </a>
                     </li>
                 </ul>
             </div>
